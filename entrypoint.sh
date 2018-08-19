45b6257340aadf95972819a59c423bfb84a5c802
Kir Belevich <kir@belevi.ch>
2018-08-19 18:24:57 +0200

    🛠 don't use `dumb-init` twice

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$(ls -A /home/chromium/.fonts/)" ]; then
   fc-cache -f -v
 fi
 
-dumb-init -- /usr/bin/chromium \
+/usr/bin/chromium \
   --disable-background-networking \
   --disable-background-timer-throttling \
   --disable-default-apps \
